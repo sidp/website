c2daa0113dcce0a7dc1d50670805b5833e562b4a
Peter Simonsson <peter@simonsson.com>
2017-02-13 22:47:55 +0100

    Added Google Analytics

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+import ReactGA from 'react-ga'
+import { config } from 'config'
+
+if (process.env.NODE_ENV === 'production') {
+	ReactGA.initialize(config.googleAnalyticsId);
+
+	exports.onRouteUpdate = (state, page, pages) => {
+		ReactGA.pageview(state.pathname);
+	}
+}
