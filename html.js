bb1626a2580efe1f5651b7c7e7064755eba168a4
Peter Simonsson <peter@simonsson.com>
2017-02-18 22:55:38 +0100

    Switched to a stateless functional component

--- html.js
+++ html.js
@@ -7,40 +7,39 @@ import typography from './utils/typography';
 
 const BUILD_TIME = new Date().getTime();
 
-module.exports = React.createClass({
-	propTypes () {
-		return {
-			body: React.PropTypes.string,
-		}
-	},
-	render () {
-		const head = Helmet.rewind();
+const Html = (props) => {
+	const head = Helmet.rewind();
 
-		let css;
-		if (process.env.NODE_ENV === 'production') {
-			css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />;
-		}
+	let css;
+	if (process.env.NODE_ENV === 'production') {
+		css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />;
+	}
 
-		return (
-			<html lang="en">
-				<head>
-					<meta charSet="utf-8" />
-					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0"
-					/>
-					{head.title.toComponent()}
-					{head.meta.toComponent()}
-					<TypographyStyle typography={typography} />
-					<GoogleFont typography={typography} />
-					{css}
-				</head>
-				<body>
-					<div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-					<script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
-				</body>
-			</html>
-		)
-	},
-});
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				{head.title.toComponent()}
+				{head.meta.toComponent()}
+				<TypographyStyle typography={typography} />
+				<GoogleFont typography={typography} />
+				{css}
+			</head>
+			<body>
+				<div id="react-mount" dangerouslySetInnerHTML={{ __html: props.body }} />
+				<script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+			</body>
+		</html>
+	)
+};
+
+Html.propTypes = {
+	body: React.PropTypes.string,
+};
+
+export default Html;
