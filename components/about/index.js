5ff1d73b6d654b7de3bf8a484155bc2f95355485
Peter Simonsson <peter@simonsson.com>
2017-03-18 19:21:40 +0100

    Implemented responsive image on about page

--- components/about/index.js
+++ components/about/index.js
@@ -4,12 +4,19 @@ import { pageProps } from '../prop-types';
 import MarkdownPage from '../markdown-page';
 
 import styles from './about.module.css';
-import portrait from '../../images/peter.jpg';
+import portrait1x from '../../images/peter-1x.jpg';
+import portrait2x from '../../images/peter-2x.jpg';
 
 const About = ({ page }) => (
 	<article role="main" className={styles['about']}>
 		<div className={styles['summary']}>
-			<img src={portrait} alt="Peter Simonsson" className={styles['portrait']} />
+			<img
+				src={portrait1x}
+				sizes="(min-width: 500px) 50vw, 100vw"
+				srcSet={`${portrait1x} 600w, ${portrait2x} 1200w`}
+				alt="Peter Simonsson"
+				className={styles['portrait']}
+			/>
 		</div>
 		<MarkdownPage
 			page={page}
