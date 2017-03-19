bc17752316932e100d8b9bb2502ec54163ee04e5
Peter Simonsson <peter@simonsson.com>
2017-03-19 21:06:15 +0100

    Open external links in new window

--- loaders/markdown-loader/index.js
+++ loaders/markdown-loader/index.js
@@ -1,25 +1,27 @@
-var frontMatter = require('front-matter')
-var markdownIt = require('markdown-it')
-var hljs = require('highlight.js')
-var objectAssign = require('object-assign')
+const frontMatter = require('front-matter');
+const markdownIt = require('markdown-it');
+const hljs = require('highlight.js');
+const objectAssign = require('object-assign');
 
-var highlight = function (str, lang) {
+const highlight = (str, lang) => {
 	if ((lang !== null) && hljs.getLanguage(lang)) {
 		try {
-			return hljs.highlight(lang, str).value
+			return hljs.highlight(lang, str).value;
 		} catch (_error) {
-			console.error(_error)
+			console.error(_error);
 		}
 	}
-	try {
-		return hljs.highlightAuto(str).value
-	} catch (_error) {
-		console.error(_error)
-	}
-	return ''
-}
 
-var md = markdownIt({
+	try {
+		return hljs.highlightAuto(str).value;
+	} catch (_error) {
+		console.error(_error);
+	}
+
+	return '';
+};
+
+const md = markdownIt({
 	html: true,
 	linkify: true,
 	typographer: true,
@@ -30,15 +32,42 @@ var md = markdownIt({
 	.use(require('markdown-it-footnote'))
 	.use(require('markdown-it-deflist'))
 	.use(require('markdown-it-abbr'))
-	.use(require('markdown-it-attrs'))
+	.use(require('markdown-it-attrs'));
+
+const defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+	return self.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+	const aHrefIndex = tokens[idx].attrIndex('href');
+	const aHrefValue = tokens[idx].attrs[aHrefIndex][1];
+
+	if (aHrefValue.indexOf('http') === 0) {
+		const aTargetIndex = tokens[idx].attrIndex('target');
+		if (aTargetIndex < 0) {
+			tokens[idx].attrPush(['target', '_blank']);
+		} else {
+			tokens[idx].attrs[aTargetIndex][1] = '_blank';
+		}
+
+		const aRelIndex = tokens[idx].attrIndex('rel');
+		if (aRelIndex < 0) {
+			tokens[idx].attrPush(['rel', 'noopener']);
+		} else {
+			tokens[idx].attrs[aRelIndex][1] = 'noopener';
+		}
+	}
+
+	return defaultRender(tokens, idx, options, env, self);
+};
 
 module.exports = function (content) {
-	this.cacheable()
-	const meta = frontMatter(content)
-	const body = md.render(meta.body)
+	this.cacheable();
+	const meta = frontMatter(content);
+	const body = md.render(meta.body);
 	const result = objectAssign({}, meta.attributes, {
 		body,
-	})
-	this.value = result
-	return `module.exports = ${JSON.stringify(result)}`
-}
+	});
+	this.value = result;
+	return `module.exports = ${JSON.stringify(result)}`;
+};
