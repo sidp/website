0e4f6fee1ba5bd545eebf3fa65a69e37cc7c34f9
Peter Simonsson <peter@simonsson.com>
2017-04-23 20:20:38 +0200

    Changed Index page to functional component

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,24 @@ import pageTitle from '../utils/page-title';
 import { getProjects } from '../utils/page-handling';
 import ProjectList from '../components/project-list';
 
-export default class Index extends React.Component {
-	render () {
-		const projects = getProjects(this.props.route.pages);
+const Index = ({ route }) => {
+	const projects = getProjects(route.pages);
 
-		return (
-			<div>
-				<Helmet
-					title={pageTitle()}
-					meta={[
-						{
-							"name": "description",
-							"content": "I'm the Technical Director of Wenderfalck in Stockholm, Sweden. This is a selection of the most popular projects I've worked on."
-						},
-					]}
-				/>
-				<h2>Projects</h2>
-				<ProjectList projects={projects} />
-			</div>
-		)
-	}
+	return (
+		<div>
+			<Helmet
+				title={pageTitle()}
+				meta={[
+					{
+						"name": "description",
+						"content": "I'm the Technical Director of Wenderfalck in Stockholm, Sweden. This is a selection of the most popular projects I've worked on."
+					},
+				]}
+			/>
+			<h2>Projects</h2>
+			<ProjectList projects={projects} />
+		</div>
+	)
 }
+
+export default Index;
