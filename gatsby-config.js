8c2041dea859aed9b66866928a721ff9f1df0d10
Peter Simonsson <peter@simonsson.com>
2017-06-01 21:26:41 +0200

    Moved site config

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	siteMetadata: {
+		siteTitle: 'Peter Simonsson',
+		linkPrefix: '',
+		googleAnalyticsId: 'UA-60527-1',
+		navigation: [
+			{
+				label: 'Projects',
+				path: '/',
+			},
+			{
+				label: 'About',
+				path: '/about/',
+			},
+		],
+	},
+};
