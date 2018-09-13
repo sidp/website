ae892e0e61bf1df5d2f51659e20e23525ea185f7
Peter Simonsson <peter@simonsson.com>
2018-09-13 16:21:48 +0200

    Moved content to submodule

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				name: 'pages',
-				path: `${__dirname}/src/pages`,
+				path: `${__dirname}/src/pages/content`,
 			},
 		},
 		{
