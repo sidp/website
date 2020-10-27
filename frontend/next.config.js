6f8f2231cd2f19f6e40b01325d966c986147e014
Peter Simonsson <peter@simonsson.com>
2020-10-27 18:00:48 +0100

    Upgrade to Next.js 10

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+	images: {
+		deviceSizes: [320, 420, 768, 1024, 1200, 1440, 1920, 2400],
+		domains: ['localhost', 'api.simonsson.com'],
+	},
 	async rewrites() {
 		return [
 			{
