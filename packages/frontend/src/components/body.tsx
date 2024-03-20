64a1e472730dec82345763796f526af9074bfe85
Peter Simonsson <peter@tulastudio.se>
2024-03-20 22:06:30 +0000

    Styling of body content

--- packages/frontend/src/components/body.tsx
+++ packages/frontend/src/components/body.tsx
@@ -17,6 +17,16 @@ const Body: FC<BodyProps> = ({ value }) => {
 			components={{
 				block: {
 					normal: (props) => <p className="max-w-3xl mb-4">{props.children}</p>,
+					h1: (props) => <h1 className="font-bold">{props.children}</h1>,
+					h2: (props) => <h2 className="font-bold">{props.children}</h2>,
+					h3: (props) => <h3 className="font-bold">{props.children}</h3>,
+				},
+				list: {
+					bullet: (props) => <ul className="ml-5">{props.children}</ul>,
+					number: (props) => <ol className="ml-5">{props.children}</ol>,
+				},
+				listItem: {
+					bullet: ({ children }) => <li className="list-disc">{children}</li>,
 				},
 				types: {
 					image: (props) => {
