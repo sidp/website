525ccc4e931088284fc41db44524ba3a456ec8fd
Peter Simonsson <peter@tulastudio.se>
2024-03-25 17:35:12 +0000

    Update sitemap

--- packages/frontend/src/pages/api/sitemap.ts
+++ packages/frontend/src/pages/api/sitemap.ts
@@ -1,8 +1,8 @@
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { NextApiHandler } from 'next';
 import { absoluteUrl } from '../../utils/url';
-import apiGet from '../../utils/api';
-import { Page, Post, Project } from '../../types';
+import { fetch } from '../../utils/sanity-fetch';
+import { Post } from '../../types';
 
 const sitemap: NextApiHandler = async (req, res) => {
 	const smStream = new SitemapStream({
@@ -11,27 +11,14 @@ const sitemap: NextApiHandler = async (req, res) => {
 
 	smStream.write({ url: absoluteUrl('/') });
 
-	const [pages, posts, projects] = await Promise.all([
-		apiGet<Page[]>('pages', { _limit: -1 }),
-		apiGet<Post[]>('posts', { _limit: -1 }),
-		apiGet<Project[]>('projects', { _limit: -1 }),
-	]);
+	const posts = await fetch<Post[]>({
+		draftMode: false,
+		query: `*[_type == "post"]`,
+	});
 
-	pages.forEach((page) => {
+	posts.forEach((page) => {
 		smStream.write({
 			url: absoluteUrl(`/${page.slug}`),
-		});
-	});
-
-	posts.forEach((post) => {
-		smStream.write({
-			url: absoluteUrl(`/posts/${post.slug}`),
-		});
-	});
-
-	projects.forEach((project) => {
-		smStream.write({
-			url: absoluteUrl(`/projects/${project.slug}`),
 		});
 	});
 
