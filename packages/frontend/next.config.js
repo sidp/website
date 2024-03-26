e498991200f64ef0a452450a3bd0c51101ac136c
Peter Simonsson <peter@tulastudio.se>
2024-03-26 12:45:39 +0000

    Update next.js configuration

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -2,6 +2,8 @@
  * @type {import('next').NextConfig}
  **/
 module.exports = {
+	reactStrictMode: true,
+	poweredByHeader: false,
 	images: {
 		formats: ['image/avif', 'image/webp'],
 		deviceSizes: [320, 420, 768, 1024, 1200, 1440, 1920, 2400, 3200],
