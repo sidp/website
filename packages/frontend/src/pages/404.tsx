3e91f4a2df4cbf15ee3b5bc7e99afa8b5f29908d
Peter Simonsson <peter@tulastudio.se>
2024-03-26 22:11:32 +0000

    Add 404 page

--- packages/frontend/src/pages/404.tsx
+++ packages/frontend/src/pages/404.tsx
@@ -1,19 +1,55 @@
-import * as React from 'react';
+import { FC } from 'react';
 import Head from 'next/head';
-import { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import { Navigation, Settings } from '../types';
+import { fetch } from '../utils/sanity-fetch';
+import title from '../utils/title';
+import Section from '../components/section';
+import Footer from '../components/footer';
+import Header from '../components/header';
 
-const ErrorPage404: NextPage = () => {
+type ErrorPage404Props = {
+	navigation: Navigation;
+	settings: Settings;
+};
+
+const ErrorPage404: FC<ErrorPage404Props> = ({ navigation, settings }) => {
 	return (
 		<>
 			<Head>
-				<title>Page not found</title>
+				<title>{title('Page not found', settings.websiteName)}</title>
 				<meta name="robots" content="noindex" />
 			</Head>
+			<Header navigation={navigation} />
+			<Section limitWidth>
+				<h1>Page not found</h1>
+				<p>The requested page could not be found.</p>
+			</Section>
 
-			<h1>Page Not Found</h1>
-			<p>The requested page could not be found.</p>
+			<Footer links={settings.socialMedia} />
 		</>
 	);
 };
 
 export default ErrorPage404;
+
+export const getStaticProps: GetStaticProps<ErrorPage404Props> = async () => {
+	const [navigation, settings] = await Promise.all([
+		fetch<Navigation>({
+			draftMode: false,
+			query: `*[_type == "navigation"][0]`,
+		}),
+		fetch<Settings>({
+			draftMode: false,
+			query: `*[_type == "settings"][0]`,
+		}),
+	]);
+
+	return {
+		props: {
+			navigation,
+			settings,
+		},
+		revalidate: 5,
+	};
+};
