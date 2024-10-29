4b04f6cfba6fd223b652c389007b9ffcd93458ae
Peter Simonsson <peter@tulastudio.se>
2024-10-29 11:34:18 +0000

    Get image size and color in body

--- packages/frontend/src/utils/sanity-data.ts
+++ packages/frontend/src/utils/sanity-data.ts
@@ -10,5 +10,14 @@ export const postFields = `
 	...,
 	image {
 		${imageFields}
+	},
+	body[] {
+		...,
+		_type == 'image' => {
+			...,
+			"width": asset->metadata.dimensions.width,
+			"height": asset->metadata.dimensions.height,
+			"color": asset->metadata.palette.dominant.background,
+		},
 	}
 `;
