b978fc5aeb36086a6a8966ee84bae1998eca8926
Peter Simonsson <peter@tulastudio.se>
2024-11-02 14:37:33 +0000

    Set up Sanity media plugin

--- packages/sanity/sanity.config.ts
+++ packages/sanity/sanity.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
-import { schemaTypes } from './schemaTypes';
-import { structure } from './structure';
 import { codeInput } from '@sanity/code-input';
 import { table } from '@sanity/table';
+import { media, mediaAssetSource } from 'sanity-plugin-media';
+import { schemaTypes } from './schemaTypes';
+import { structure } from './structure';
 import Icon from './components/icon';
 
 const singletonActions = new Set(['publish', 'discardChanges', 'restore']);
@@ -19,12 +20,35 @@ export default defineConfig({
 	projectId: 'hxcrb1u6',
 	dataset: 'production',
 
-	plugins: [structureTool({ structure }), visionTool(), codeInput(), table()],
+	plugins: [
+		structureTool({ structure }),
+		visionTool(),
+		codeInput(),
+		table(),
+		media(),
+	],
 
 	schema: {
 		types: schemaTypes,
 		templates: (templates) =>
 			templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
+	},
+
+	form: {
+		file: {
+			assetSources: (previousAssetSources) => {
+				return previousAssetSources.filter(
+					(assetSource) => assetSource !== mediaAssetSource,
+				);
+			},
+		},
+		image: {
+			assetSources: (previousAssetSources) => {
+				return previousAssetSources.filter(
+					(assetSource) => assetSource === mediaAssetSource,
+				);
+			},
+		},
 	},
 
 	document: {
